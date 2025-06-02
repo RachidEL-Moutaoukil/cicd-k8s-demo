ea5db8a5767eb2fcb30caa558f80b7000f79d713
RachidEL-Moutaoukil <rachid.elmoutaoukil@ynov.com>
2025-06-02 03:35:50 +0200

    add commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const version = process.env.VERSION || '1.0.0';
+app.get('/', (req, res) => {
+    res.send(`Hello Kubernetes  CI/CD! Version: ${version}`);
+});
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
---
+++ test/app.test.js
@@ -0,0 +1,10 @@
+console.log('Running simple test');
+// Ici vous pourriez avoir des tests réels avec Jest, Mocha, etc.
+// Pour ce TP, nous simulons juste que les tests passent
+if (true) {
+console.log('Tests passed');
+process.exit(0);
+} else {
+    console.log('Tests failed');
+    process.exit(1);
+}
