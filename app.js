5c608012f2c5b456e45edd60e84c84e3239d26cf
RachidEL-Moutaoukil <rachid.elmoutaoukil@ynov.com>
2025-06-02 12:10:11 +0200

    add cicd

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 const version = process.env.VERSION || '1.0.0';
 app.get('/', (req, res) => {
-    res.send(`Hello Kubernetes  CI/CD! Version: ${version}`);
+    res.send(`salut tout le monde Version: ${version}`);
 });
 app.get('/health', (req, res) => {
-    res.status(200).send('cool');
+    res.status(200).send('good');
 });
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
